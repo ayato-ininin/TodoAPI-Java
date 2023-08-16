b02412e951a89fd24a257ccc49652811f3f7fdf1
ayato yamauchi <>
2023-08-16 09:41:04 +0900

    レスポンスをjavaオブジェクト(DTO)かつJSONで返す

--- src/main/java/com/example/todoapi/controller/sample/SampleController.java
+++ src/main/java/com/example/todoapi/controller/sample/SampleController.java
@@ -4,14 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+
 //↓合成アノテーションといい、複数のアノテーションをまとめて付与できる
+//この中に@ResponseBodyが含まれており、このクラス内の返り値はresponse bodyに変換される
 @RestController
 //↓GetMappingやPostMappingなどのアノテーションを使うときに、共通パスを指定できる
 @RequestMapping("/samples")
 public class SampleController {
 
     @GetMapping
-    public String index() {
-        return "Hello World";
+    public SampleDto index() {
+        //spring bootプロジェクト内のjacksonというライブラリがJavaオブジェクトを自動的にJSONに変換してくれる
+        return new SampleDto("OK", LocalDateTime.now());
     }
 }
---
+++ src/main/java/com/example/todoapi/controller/sample/SampleDto.java
@@ -0,0 +1,24 @@
+package com.example.todoapi.controller.sample;
+
+import java.time.LocalDateTime;
+
+// レスポンス用のDTO
+public class SampleDto {
+
+    private String content;
+    private LocalDateTime timestamp;
+
+    // コンストラクタ
+    public SampleDto(String content, LocalDateTime timestamp) {
+        this.content = content;
+        this.timestamp = timestamp;
+    }
+
+    // getter
+    public String getContent() {
+        return content;
+    }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+}
