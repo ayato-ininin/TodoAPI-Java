67340623f1a24dd54ad34eefee9bd0284dd7bf40
ayato yamauchi <>
2023-08-16 21:52:39 +0900

    独自例外ハンドラにて、jsonボディを返却する用変更

--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todoapi.model.ResourceNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +14,11 @@ public class CustomExceptionHandler {
 
     // ↓下記アノテーションで、TaskEntityNotFoundExceptionが発生した場合に、404エラーを返すハンドラー定義
     @ExceptionHandler(TaskEntityNotFoundException.class)
-    public ResponseEntity<Void> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
-        return ResponseEntity.notFound().build();
+    public ResponseEntity<ResourceNotFoundError> handleTaskEntityNotFoundException(TaskEntityNotFoundException e) {
+        var error = new ResourceNotFoundError();
+        error.setDetail(e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(error);
     }
 }
