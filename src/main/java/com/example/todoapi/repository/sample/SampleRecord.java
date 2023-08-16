16c3d4e65e266777daac8cdd98f85e4633312356
ayato yamauchi <>
2023-08-16 10:57:12 +0900

    データアクセス層のレポジトリとレコードの作成

---
+++ src/main/java/com/example/todoapi/repository/sample/SampleRecord.java
@@ -0,0 +1,14 @@
+package com.example.todoapi.repository.sample;
+
+public class SampleRecord {
+
+    private String content;
+
+    public SampleRecord(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
--- src/main/java/com/example/todoapi/service/sample/SampleService.java
+++ src/main/java/com/example/todoapi/service/sample/SampleService.java
@@ -1,12 +1,19 @@
 package com.example.todoapi.service.sample;
 
+import com.example.todoapi.repository.sample.SampleRepository;
 import org.springframework.stereotype.Service;
 
 //↓DIするためのアノテーション(bean登録)
 @Service
 public class SampleService {
 
+    private final SampleRepository repository;
+    public SampleService(SampleRepository repository) {
+        this.repository = repository;
+    }
+
     public SampleEntity find() {
-        return new SampleEntity("Hello World!");
+        var record = repository.select();
+        return new SampleEntity(record.getContent()) ;
     }
 }
