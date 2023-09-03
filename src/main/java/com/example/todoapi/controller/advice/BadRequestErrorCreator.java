c9d747a7577c368303c8b07e130479fc307a2fef
ayato yamauchi <>
2023-09-03 19:46:30 +0900

    add スキーマにバリデーション追加（これだけでうまく弾いてくれる）

--- src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
+++ src/main/java/com/example/todoapi/controller/advice/BadRequestErrorCreator.java
@@ -5,23 +5,18 @@ import com.example.todoapi.model.InvalidParam;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 public class BadRequestErrorCreator {
     public static BadRequestError from(MethodArgumentNotValidException ex) {
-        var invalidParamList = createInvalidParamList(ex);
-        var error = new BadRequestError();
-        error.setInvalidParams(invalidParamList);
-        return error;
-    }
-
-    private static List<InvalidParam> createInvalidParamList(MethodArgumentNotValidException ex) {
-        return ex.getFieldErrors()
+        var invalidParamList = ex.getFieldErrors()
                 .stream()
                 // .map(fieldError -> createInvalidParam(fieldError))の書き換え(メソッド参照)
                 .map(BadRequestErrorCreator::createInvalidParam)
                 .collect(Collectors.toList());
+        var error = new BadRequestError();
+        error.setInvalidParams(invalidParamList);
+        return error;
     }
 
     // 抜き出したい関数の部分を選択して、コマンド + option + Mすると切り出せる。
