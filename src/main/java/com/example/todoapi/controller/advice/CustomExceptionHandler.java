e694c7be38522e7a53ababf9541a1426b888c730
ayato yamauchi <>
2023-09-03 19:33:29 +0900

    Fieldが正しくない場合のCustomErrorHandler作成（例外キャッチ）

--- src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
+++ src/main/java/com/example/todoapi/controller/advice/CustomExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.example.todoapi.controller.advice;
 
+import com.example.todoapi.model.BadRequestError;
 import com.example.todoapi.model.ResourceNotFoundError;
 import com.example.todoapi.service.task.TaskEntityNotFoundException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 // 下記は、途中でエラーを差し込むためのクラス
 // これを定義するだけでエラーをキャッチして、ここからレスポンスを返却できる
 @RestControllerAdvice
-public class CustomExceptionHandler {
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
     // ↓下記アノテーションで、TaskEntityNotFoundExceptionが発生した場合に、404エラーを返すハンドラー定義
     @ExceptionHandler(TaskEntityNotFoundException.class)
@@ -20,5 +26,18 @@ public class CustomExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(error);
+    }
+
+    // POSTメソッドで引数が不正な場合、handleMethodArgumentNotValidが呼ばれるため、
+    // ここでエラーをキャッチして、独自のエラーレスポンスを返却する
+    @Override
+    protected org.springframework.http.ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        var error = BadRequestErrorCreator.from(ex);
+        return ResponseEntity.badRequest().body(error);
     }
 }
