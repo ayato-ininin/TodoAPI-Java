eaf61d33babaee6a70d53f2a918c8483db30b424
ayato yamauchi <>
2023-09-03 20:26:49 +0900

    add limit offsetを追加し、ページングできるよう修正

--- src/main/java/com/example/todoapi/controller/task/TaskController.java
+++ src/main/java/com/example/todoapi/controller/task/TaskController.java
@@ -38,8 +38,8 @@ public class TaskController implements TasksApi {
     }
 
     @Override
-    public ResponseEntity<TaskListDTO> listTasks() {
-        var entityList = taskService.find();
+    public ResponseEntity<TaskListDTO> listTasks(Integer limit, Long offset) {
+        var entityList = taskService.find(limit, offset);
         var dtoList = entityList.stream()
                 .map(TaskController::toTaskDto)
                 .collect(Collectors.toList());
