405f43b62c53087bff3d91cbf6fc3e172c2fb30b
ayato yamauchi <>
2023-10-26 22:16:51 +0900

    add データベースのuser初期値追加

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,9 @@ INSERT INTO tasks (title) VALUES ('Task2');
 INSERT INTO tasks (title) VALUES ('Task3');
 INSERT INTO tasks (title) VALUES ('Task4');
 INSERT INTO tasks (title) VALUES ('Task5');
+
+INSERT INTO users (name) VALUES ('Name1');
+INSERT INTO users (name) VALUES ('Name2');
+INSERT INTO users (name) VALUES ('Name3');
+INSERT INTO users (name) VALUES ('Name4');
+INSERT INTO users (name) VALUES ('Name5');
